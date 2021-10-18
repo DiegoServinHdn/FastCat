aeb78b14ee8b613a61b86ec7494ccae198d86ae4
Diego Servin <Diegoservinhdn@gmail.com>
2021-10-17 19:32:56 -0500

    Add model Workspace

--- FastCatBack/api/models.py
+++ FastCatBack/api/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.db.models import deletion
+import string 
+import random
+
+def generate_unique_code(lenght=5):
+    while True:
+        code =  ''.join(random.choices(string.ascii_uppercase + string.digits, k=lenght))
+        if Workspace.objects.filter(code=code).count() == 0:
+            return code
 
 # Create your models here.
 
 class Workspace(models.Model):
     code = models.CharField(max_length=5, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
+    guest_can_edit = models.BooleanField(null=False, default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
